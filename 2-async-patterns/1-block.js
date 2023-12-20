6d451922883fa6da4989293cc724f578e32b993a
deathnote <127240347+thakurshashant@users.noreply.github.com>
2023-12-20 11:55:49 +0530

    4- clean way to write async code having callbacks

---
+++ 2-async-patterns/1-block.js
@@ -0,0 +1,40 @@
+//solution for cleaner async code (not messy as usual)
+//util is used to convert any function into a promised  based function
+
+
+const {readFile ,writeFile} = require ('fs').promises
+
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
+
+const start = async () => {
+    try{
+        const first = await readFile('./content/first.txt' ,'utf8');
+        const second = await readFile('./content/second.txt','utf8');
+        await writeFile('./content/result-mind-grenade.txt' , `This is awesome : ${first} , ${second} , {flag : 'a'}`);
+        console.log(first ,second);
+    }catch(err){
+        console.log(err)
+
+    }
+}
+
+start()
+
+
+// const getText = (path) => {
+//     return new Promise( (resolve,reject) => {
+//         readFile(path ,'utf8' , (err, data) =>{
+//             if(err){
+//                 reject(err)
+//             }else{
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
+
+// getText('./content/first.txt')
+// .then((result) => console.log(result))
+// .catch((err) => console.log(err))
